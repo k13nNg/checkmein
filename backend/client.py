914559ef48155937c5a73aabd1301ad9e52ee5d9
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2023-06-05 14:04:58 -0400

    added register function

--- backend/client.py
+++ backend/client.py
@@ -1,10 +1,36 @@
-from connect_database import get_database
+from database_funcs import *
+import bcrypt
 
 db = get_database()
 
-student_ids = db["student_ids"]
+student_ids_col = db["student_ids"]
 
-item_details = student_ids.find()
+print("Program started")
 
-for i in item_details:
-    print(i)
+while True:
+    choice = int(input("Enter 1 to register a student, 2 to check in\n"))
+
+    if choice == 1:
+        student_name = input("Please enter the student's name\n")
+        student_id = input("Please enter student's id\n")
+
+        bytes = student_id.encode("utf-8")
+        salt = bcrypt.gensalt()
+
+        hashed_id = bcrypt.hashpw(bytes, salt)
+
+        reg_student(student_name=student_name, student_id=hashed_id, collection=student_ids_col)
+
+    elif choice == 2:
+        student_id = input("Please tap your card\n")
+
+        result = get_student_by_id(student_id=student_id, collection=student_ids_col)
+
+        if (result != False):
+            print("Hello " + result["student_name"] + ", you are checked in!" )
+        
+        else:
+            print("Error: Student does not exist")
+    
+    else:
+        print("Please enter either 1 or 2")
--- backend/database_funcs.py
+++ backend/database_funcs.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
+import bcrypt
 import os
 
 load_dotenv()
@@ -8,4 +9,26 @@ load_dotenv()
 def get_database():
 
     client = MongoClient(os.getenv("CHECKMEIN_DB_URI"), server_api=ServerApi('1'))
+
     return client[os.getenv("CHECKMEIN_COL_NAME")]
+
+# get_student_by_id(id, collection) returns a Cursor object that points to specific document in 
+# the MongoDB database, returns -1 if the student is not found
+# (str, collection) -> Cursor
+def get_student_by_id(student_id, collection):
+    students = collection.find()
+    student_id = student_id.encode("utf-8")
+
+    for i in students:
+        if bcrypt.checkpw(student_id, i["student_id"]):
+            return i
+
+    return False
+
+# reg_student(student_name, student_id, collection) insert a student into the database
+# (str, binary, collection) -> void
+def reg_student(student_name, student_id, collection):
+    query = {"student_id": student_id, "student_name": student_name}
+
+    collection.insert_one(query)
+
