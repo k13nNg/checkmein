0a9bbc9945569e23f865fb9f5d2de765bf7a8132
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2023-06-22 20:54:37 -0400

    fixed the local network issue

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const student_checkin = axios.create({
-    baseURL: "http://localhost:8000/api/v1/students",
+    baseURL: "http://192.168.137.234:8000/api/v1/students",
     headers: {
         "Content-type": "application/json"
     }
